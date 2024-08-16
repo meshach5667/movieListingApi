4ab398ac947d62c3ba8c6f16e1539141b8bf6438
meshach5667 <meshachzakumi@gmail.com>
2024-08-16 16:20:17 +0100

    boom

--- oauth2.py
+++ oauth2.py
@@ -1,23 +1,22 @@
-from fastapi import Depends, HTTPException, status
+# oauth2.py
+from fastapi import HTTPException, status, Depends
 from fastapi.security import OAuth2PasswordBearer
-import jwt
-from jwt_token import SECRET_KEY, verify_token
+import database
 from schemas import schemas
-from schemas.schemas import TokenData
-import models.models as models  # Ensure this file contains Pydantic models
-import database.database as database
+from jwt_token import verify_token
 from motor.motor_asyncio import AsyncIOMotorDatabase
 from bson import ObjectId
+from database.database import get_db
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
-async def get_current_user(token: str = Depends(oauth2_scheme), db: AsyncIOMotorDatabase = Depends(database.get_db)):
+async def get_current_user(token: str = Depends(oauth2_scheme), db: AsyncIOMotorDatabase = Depends(database.database.get_db)) -> schemas.User:
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
-
+    
     # Verify the token and extract token data
     token_data = verify_token(token, credentials_exception)
     
@@ -27,5 +26,11 @@ async def get_current_user(token: str = Depends(oauth2_scheme), db: AsyncIOMotor
     if user is None:
         raise credentials_exception
     
-    # Assuming User is a Pydantic model that matches the schema of the user document
-    return schemas.User(**user)
+    
+    user_data = {
+        "id": str(user["_id"]),  # Convert ObjectId to string
+        **{key: user[key] for key in user if key != "_id"}  # Include other fields
+    }
+    
+    # Create a Pydantic model instance
+    return schemas.User(**user_data)
